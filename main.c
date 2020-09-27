b892c040c59bec75a27e1b67f2f959a37e507921
ThitipongPhasom <titipong.k5143@gmail.com>
2020-09-27 13:16:49 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+  int a,x;
+  printf("Input A\n");
+  scanf("%d",&a);
+  if(a > 0)
+  {
+  x = a*15;
+  }
+  else
+  {
+  x = a*(-15);
+  }
+printf("%d",x);
+  return 0;
+}
